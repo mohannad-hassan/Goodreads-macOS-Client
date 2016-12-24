6a6a68a76826f7d3366fe4c41002e414f63f7999
Mohannad A. Hassan <mohannad.hassan@badrit.com>
2016-12-24 11:27:31 +0200

    Create AuthenticationCenter
    To be used in interface of LoginInteractort

---
+++ Goodreads macOS Client/AuthenticationCenter.swift
@@ -0,0 +1,13 @@
+//
+//  AuthenticationCenter.swift
+//  Goodreads macOS Client
+//
+//  Created by Mohannad Hassan on 12/24/16.
+//  Copyright © 2016 Mohannad Hassan. All rights reserved.
+//
+
+import Cocoa
+
+class AuthenticationCenter: NSObject {
+
+}
