9f04cd7037416844708ce81ab8149fe98ce06eb5
Mohannad A. Hassan <mohannad.hassan@badrit.com>
2016-12-24 07:27:00 +0200

    Initial Commit

---
+++ Goodreads macOS Client/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Goodreads macOS Client
+//
+//  Created by Mohannad Hassan on 12/24/16.
+//  Copyright © 2016 Mohannad Hassan. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Goodreads macOS Client/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Goodreads macOS Client
+//
+//  Created by Mohannad Hassan on 12/24/16.
+//  Copyright © 2016 Mohannad Hassan. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
