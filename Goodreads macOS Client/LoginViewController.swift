a12b94af8b919ded497d0781fa8c36fba951b629
Mohannad A. Hassan <mohannad.hassan@badrit.com>
2016-12-24 07:56:19 +0200

    Create LoginViewControllerTests with tests

---
+++ Goodreads macOS Client/LoginViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LoginViewController.swift
+//  Goodreads macOS Client
+//
+//  Created by Mohannad Hassan on 12/24/16.
+//  Copyright © 2016 Mohannad Hassan. All rights reserved.
+//
+
+import Cocoa
+
+class LoginViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
